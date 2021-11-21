8e24cd97c8148b88a88860c7eb8db562f3f6f427
Anjali Soni <anjalipatadiya@gmail.com>
2021-11-21 00:00:08 -0700

    Implemented coordinates lat and long

--- iss.js
+++ iss.js
@@ -11,7 +11,7 @@ const request = require('request');
  */
 
 
-const fetchMyIP = function(callback) {
+/* const fetchMyIP = function(callback) {
 
   request('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) {
@@ -27,6 +27,24 @@ const fetchMyIP = function(callback) {
     callback(null, ipData);
 
   });
+}; */
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const {latitude, longitude} = JSON.parse(body);
+   
+    callback({latitude, longitude}, null);
+    
+  });
 };
 
-module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
